01d6813c78b29da4d483ba81aec32035a721c76a
balanceM <286515744@qq.com>
2025-11-27 11:20:34 +0800

    task3

--- contracts/task3/BeggingContract.sol
+++ contracts/task3/BeggingContract.sol
@@ -4,13 +4,16 @@ pragma solidity ^0.8.12;
 contract BeggingContract {
     address public owner;
     mapping(address => uint256) public donatorTotal;
-
-    struct Donation {
-        address donator;
-        uint256 amount;
-        uint256 timestamp;
-    }
-    Donation[] public donationRecords;
+    // struct Donation {
+    //     address donator;
+    //     uint256 amount;
+    //     uint256 timestamp;
+    // }
+    // Donation[] public donationRecords;
+    event Donation(address donator, uint256 value);
+    uint256 top3MinAmount; // 进入前3的最少金额
+    uint256 top3MinIndex; // 进入前3的最少金额的对应地址索引
+    address[3] public top3Donators; // 捐赠金额最多的前3个地址
 
     constructor() {
         owner = msg.sender;
@@ -21,15 +24,61 @@ contract BeggingContract {
         _;
     }
 
-    function donate() external payable {
+    uint256 public constant TARGET_TIMESTAMP = 1735702861;
+    modifier timeLimit() {
+        require(block.timestamp > TARGET_TIMESTAMP, "donate start after 2025-01-01 01:01:01 UTC");
+        _;
+    }
+
+    function donate() external payable timeLimit {
         require(msg.value > 0, "value must be greater than 0");
 
-        donationRecords.push(Donation({
-            donator: msg.sender,
-            amount: msg.value,
-            timestamp: block.timestamp
-        }));
+        // donationRecords.push(Donation({
+        //     donator: msg.sender,
+        //     amount: msg.value,
+        //     timestamp: block.timestamp
+        // }));
+        emit Donation(msg.sender, msg.value);
         donatorTotal[msg.sender] += msg.value;
+
+        uint256 curAmount;
+        address curDonator;
+        if(donatorTotal[msg.sender] > top3MinAmount) {
+            bool CountGreater3 = true;
+            for (uint256 i=0; i < 3; i++) {
+                if (top3Donators[i] == address(0)) { // 捐赠者还没达到3个
+                    CountGreater3 = false;
+
+                    top3Donators[i] = msg.sender;
+                    if(i != 2) break; // 后续还会有0地址时
+
+                    // 最后一位是0地址时，设置当前捐赠者时
+                    top3MinAmount = donatorTotal[top3Donators[0]];
+                    for(uint256 j=1; j<3; j++) {
+                        curDonator = top3Donators[j];
+                        curAmount = donatorTotal[curDonator];
+                        if (top3MinAmount > curAmount) {
+                            top3MinAmount = curAmount;
+                            top3MinIndex = j;
+                        }
+                    }
+                }
+            }
+
+            if (CountGreater3) { // 捐赠者达到3个,执行
+                top3Donators[top3MinIndex] = msg.sender;
+                top3MinAmount = donatorTotal[msg.sender];
+                for (uint256 i=0; i < 3; i++) {
+                    if(i == top3MinIndex) continue;
+
+                    curAmount = donatorTotal[top3Donators[i]];
+                    if(top3MinAmount > curAmount) {
+                        top3MinAmount = curAmount;
+                        top3MinIndex = i;
+                    }
+                }
+            }
+        }
     }
 
     function withdraw() external onlyOwner {
